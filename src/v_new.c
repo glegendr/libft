25008b8b0e66e9900ca6b8551dae4351ebbd1fb9
Gaspard LEGENDRE <glegendr@e1r10p9.42.fr>
2017-12-12 22:01:06 +0100

    Add v_new.c

---
+++ src/v_new.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   v_new.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: glegendr <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/12/12 21:04:30 by glegendr          #+#    #+#             */
+/*   Updated: 2017/12/12 21:12:28 by glegendr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "vector.h"
+
+t_vec		v_new(size_t elem_size)
+{
+	t_vec vec;
+
+	if (vec.private_content = malloc(elem_size * NB_ELEM_INI))
+		return (0);
+	vec.private_elem_nb = 0;
+	vec.private_elem_size = elem_size;
+	vec.private_elem_cap = NB_ELEM_INI;
+	return (vec);
+}
