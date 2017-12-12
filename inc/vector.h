1e37db3c5d0ccefcd3ea876a26e7bba44918aca6
Gaspard LEGENDRE <glegendr@e1r10p9.42.fr>
2017-12-12 21:02:24 +0100

    Add the .h for vector

---
+++ inc/vector.h
@@ -0,0 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   vector.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: glegendr <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/12/12 20:26:53 by glegendr          #+#    #+#             */
+/*   Updated: 2017/12/12 21:00:51 by glegendr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef VECTOR_H
+# define VECTOR_H
+#include <stddef.h>
+
+typedef struct	s_vec
+{
+void			*private_content;
+size_t			private_elem_nb;
+size_t			private_elem_size;
+size_t			private_elem_cap;
+}				t_vec;
+
+
+/*
+** Create a new vector.
+**
+** Param:
+**    elem_size: the size of each element of the vector.
+*/
+t_vec			v_new(size_t elem_size);
+
+/*
+** Add a new element at the end of the vector.
+**
+** Params:
+**    vec: the current vector.
+**    elem: the element to add at the end of vector.
+*/
+void			v_push(t_vec *vec, void *elem);
+
+/*
+** Get the pointer of element pointed by index.
+**
+** Params:
+**    vec: the current vector.
+**    index: the index.
+*/
+void			*v_get(t_vec *vec, size_t index);
+
+/*
+** Return the element number in the vector.
+**
+** Param:
+**    vec: the current vector.
+*/
+size_t			v_size(t_vec *vec);
+#endif
