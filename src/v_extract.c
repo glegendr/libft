34ecedb861f3de595afcbbf2ee9dc9fc0ce773a8
Gaspard LEGENDRE <glegendr@e1r6p15.42.fr>
2017-12-18 23:06:00 +0100

    Del

--- src/v_extract.c
+++ src/v_extract.c
@@ -6,7 +6,7 @@
 /*   By: glegendr <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/13 14:36:50 by glegendr          #+#    #+#             */
-/*   Updated: 2017/12/18 17:36:18 by glegendr         ###   ########.fr       */
+/*   Updated: 2017/12/18 23:03:54 by glegendr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,13 +24,10 @@ void		*v_extract(t_vec *vec, size_t index)
 	pos = v_get(vec, index);
 	ft_memcpy(tmp, pos, vec->private_elem_size);
 	i = 0;
-	//printf("%s\n", ((char*)vec->private_content));
 	while (i < vec->private_elem_nb - 1 && ((char *)vec->private_content)[i + 1])
 	{
-	//	printf("%c\t%c\n", ((char *)vec->private_content)[i], ((char *)vec->private_content)[i + 1]);
 		((char *)vec->private_content)[i] =
 			((char *)vec->private_content)[i + 1];
-	//	printf("%c\there\n", ((char *)vec->private_content)[i]);
 		++i;
 	}
 	((char *)vec->private_content)[i] = 0;
